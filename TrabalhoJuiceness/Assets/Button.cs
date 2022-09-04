c231cf8c940ce469747c1cf57a42d789cbf81fc7
Anderson Paim <andersonpaim15@outlook.com>
2022-09-04 18:30:36 -0300

    Added cube animation and added bomb fire particle

--- TrabalhoJuiceness/Assets/Button.cs
+++ TrabalhoJuiceness/Assets/Button.cs
@@ -4,11 +4,13 @@ using DG.Tweening;
 public class Button : MonoBehaviour
 {
     [SerializeField] private ExplosionManager _explosionManager;
+    [SerializeField] private ParticleSystem _buttonParticle;
 
     private void OnTriggerEnter(Collider other)
     {
         transform.DOLocalMoveY(-1.4f, 0.2f);
         _explosionManager.CreateBomb();
+        _buttonParticle.Play();
     }
 
     private void OnTriggerExit(Collider other)
